51fb7a5ebf0519143a07617a063d8e354b1faf14
Kubzel <kzelazko32@gmail.com>
2023-04-18 21:31:35 +0200

    README file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow, Menu, MenuItem } = require('electron');
 const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
@@ -25,9 +25,7 @@ const createWindow = () => {
  // mainWindow.webContents.openDevTools();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+
 app.on('ready', createWindow);
 
 const menubar = [
